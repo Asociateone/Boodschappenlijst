4ab469771b86eba96e5b32192da6ff7c4dcc471d
Rainaldo <61870393+Asociateone@users.noreply.github.com>
2022-06-14 20:44:22 +0200

    code styling except

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('products', [ProductController::class, 'index'])->name('products');
-
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::apiResource('user', UserController::class)->middleware(['auth:api', 'userBelongsToPage'])->names('user')->except(['index', 'create']);
+Route::get('products', [ProductController::class, 'index'])->name('products');
+
+Route::apiResource('user', UserController::class)
+    ->middleware(['auth:api', 'userBelongsToPage'])
+    ->names('user')
+    ->except(['index', 'create']);
 
 Route::prefix('user/{user}')->middleware(['auth:api', 'userBelongsToPage'])->group(function () {
-    Route::apiResource('shoppinglist', ShoppinglistController::class)->names('shoppinglist')->except(['update']);
+    Route::apiResource('shoppinglist', ShoppinglistController::class)
+        ->names('shoppinglist')
+        ->except(['update']);
 
-    Route::apiResource('shoppinglist/{shoppinglist}/items', ShoppinglistItemController::class);
+    Route::apiResource('shoppinglist/{shoppinglist}/items', ShoppinglistItemController::class)
+        ->names('shoppinglistitems')
+        ->except(['show', 'index', 'store']);
 });
