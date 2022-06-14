ea2c825dde27f2ffc663992a6b00d8f3a4e45271
Rainaldo <61870393+Asociateone@users.noreply.github.com>
2022-06-14 18:38:21 +0200

    removed comment added homecontroller

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ShoppinglistController;
 use App\Http\Controllers\ShoppingListItemController;
-use App\Http\Controllers\UserController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -24,7 +13,7 @@ Route::get('/', function () {
 
 Route::get('products', [ProductController::class, 'index'])->name('products');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::apiResource('user', UserController::class)->middleware(['auth:api', 'userBelongsToPage'])->names('user')->except(['index', 'create']);
 
